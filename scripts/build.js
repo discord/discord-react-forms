9547f5269bae175377102d832047875487118ca7
Michael Peterson <peterson@discordapp.com>
2016-11-02 17:20:18 -0700

    Don't build the old way

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,1 @@
-const webpack = require('webpack');
-const webpackConfig = require('./helpers/webpack.config');
 
-webpack(webpackConfig.getBuildConfig(), (err, stats) => {
-  if (err) {
-    console.error(err);
-  }
-  else {
-    console.log(stats.toString({colors: true}));
-  }
-});
